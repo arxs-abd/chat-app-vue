63e150bbd84802476a4ae079655f92f23fa0b90c
arxs-abd <70242781+arxs-abd@users.noreply.github.com>
2023-05-26 01:24:31 +0800

    update pusher init

--- src/helpers/constant.js
+++ src/helpers/constant.js
@@ -2,5 +2,10 @@ const config = {
   url: {
     api: import.meta.env.VITE_BASE_URL,
   },
+  pusher: {
+    api_key: import.meta.env.VITE_PUSHER_API_KEY,
+    cluster: import.meta.env.VITE_PUSHER_CLUSTER,
+    auth_url: import.meta.env.VITE_PUSHER_AUTH_URL,
+  },
 }
 export { config }
